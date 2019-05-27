b30059594b3ee8d8a6a45d601f7c09259391000b
Md Rafid Mushfique <49343788+RafidMushfique@users.noreply.github.com>
2019-05-28 03:40:13 +0600

    4th commit

--- js/script.js
+++ js/script.js
@@ -18,4 +18,21 @@ function change()
     }
 
 }
+function calculate() {
+    var values = document.getElementById("box1").value.split(/,/g);
+    var sum = values.reduce(function(a, b) { return parseInt(a) + parseInt(b); });
+    document.getElementById("max").innerHTML = values.max();
+    document.getElementById("min").innerHTML = values.min();
+    document.getElementById("sum").innerHTML = sum;
+    document.getElementById("avg").innerHTML = sum / values.length;
+    document.getElementById("reverse").innerHTML = values.reverse().join(',');
+  }
+  
+  Array.prototype.max = function() {
+    return Math.max.apply(null, this);
+  };
+  
+  Array.prototype.min = function() {
+    return Math.min.apply(null, this);
+  };
               
