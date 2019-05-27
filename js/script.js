368a8dc40859444e445b0a0cafa378206cfdfcc0
Md Rafid Mushfique <49343788+rafidmushfique@users.noreply.github.com>
2019-05-28 02:09:05 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,14 +7,14 @@ function change()
     var x=document.getElementById("in_lb").value;
     var num;
     num = x*0.4536;
-    document.getElementById("demo1").innerHTML=num+" kilograms";
+    document.getElementById("demo1").innerHTML=num + " kilograms";
     }
     else if(y=="kg_to_lb")
     {
         var x=document.getElementById("in_lb").value;
         var num;
         num = x*2.2046;
-        document.getElementById("demo1").innerHTML=num+" pounds";
+        document.getElementById("demo1").innerHTML=num + " pounds";
     }
 
 }
